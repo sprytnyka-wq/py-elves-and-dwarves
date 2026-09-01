b50ba105791b8192784391a044472b953b804515
Karim Mouminoun <sprytnyka@gmail.com>
2026-09-01 10:31:34 +0200

    project5

--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -8,13 +8,5 @@ class Elf(Player, ABC):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
-    @abstractmethod
-    def get_rating(self) -> int:
-        pass
-
-    @abstractmethod
-    def player_info(self) -> str:
-        pass
-
     def play_elf_song(self) -> None:
         print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
