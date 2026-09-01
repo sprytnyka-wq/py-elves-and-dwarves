6353f3f1d452b320149e8bfdadf470c956723e2d
Karim Mouminoun <sprytnyka@gmail.com>
2026-09-01 10:23:43 +0200

    project4

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,10 +1,20 @@
+from abc import abstractmethod
 from app.players.player import Player
 
 
 class Dwarf(Player):
+    @abstractmethod
     def __init__(self, nickname: str, favourite_dish: str) -> None:
         super().__init__(nickname)
         self._favourite_dish = favourite_dish
+
+    @abstractmethod
+    def get_rating(self) -> int:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> str:
+        pass
 
     def eat_favourite_dish(self) -> None:
         print(f"{self.nickname} is eating {self._favourite_dish}")
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,10 +1,20 @@
+from abc import abstractmethod, ABC
 from app.players.player import Player
 
 
-class Elf(Player):
+class Elf(Player, ABC):
+    @abstractmethod
     def __init__(self, nickname: str, musical_instrument: str) -> None:
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
+
+    @abstractmethod
+    def get_rating(self) -> int:
+        pass
+
+    @abstractmethod
+    def player_info(self) -> str:
+        pass
 
     def play_elf_song(self) -> None:
         print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
